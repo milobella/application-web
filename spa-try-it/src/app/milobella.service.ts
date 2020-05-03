6f753006edde471a6dad92d3cf9eaf016bbffc06
Celian Garcia <celian.garcia1@gmail.com>
2020-05-03 19:40:32 +0200

    Fixed the use of token in the milobella request

--- spa-try-it/src/app/milobella.service.ts
+++ spa-try-it/src/app/milobella.service.ts
@@ -1,22 +1,41 @@
-import {Injectable} from '@angular/core';
-import {MilobellaRequest} from './milobella-request';
-import {MilobellaResponse} from './milobella-response';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { MilobellaRequest } from './milobella-request';
+import { MilobellaResponse } from './milobella-response';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { User } from '../../../spa-login/src/app/login/user';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MilobellaService {
-  httpOptions = {
-    headers: new HttpHeaders({
-      // 'Access-Control-Allow-Origin': '*'
-    })
-  };
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) {
+  }
 
   invoke(request: MilobellaRequest): Observable<MilobellaResponse> {
-    return this.http.post<MilobellaResponse>('talk/text', request, this.httpOptions);
+    const user: User = JSON.parse(localStorage.getItem('currentUser'));
+    if (!user || !user.token) {
+      this.redirectToLogin();
+    }
+    return this.http.post<MilobellaResponse>('talk/text', request, {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + user.token
+      })
+    }).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status && err.status === 401) {
+          this.redirectToLogin();
+        } else {
+          throwError(err);
+        }
+        return of({vocal: ''});
+      })
+    );
+  }
+
+  private redirectToLogin() {
+    this.router.navigate(['/login']);
   }
 }
