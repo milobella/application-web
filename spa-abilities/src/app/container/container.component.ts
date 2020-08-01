b6623a085ab6dc39d22b9ca9aac84901ef1dd244
Celian Garcia <celian.garcia1@gmail.com>
2020-08-01 16:21:41 +0200

    Deduplicate abilities from configuration

--- spa-abilities/src/app/container/container.component.ts
+++ spa-abilities/src/app/container/container.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Outlet } from '../table/outlet.model';
 import { AbilityService } from '../api/v1/ability.service';
-import { Abilities } from '../api/v1/ability.model';
+import { Abilities, Ability } from '../api/v1/ability.model';
 
 @Component({
   selector: 'abilities-container',
@@ -19,11 +19,39 @@ export class ContainerComponent implements OnInit {
     },
   ];
 
+  private static splitByIntent(abilities: Ability[]): Ability[] {
+    return abilities;
+  }
+  private static mergeByNameAndHostAndPort(abilities: Ability[]): Ability[] {
+    const resultMap = new Map<string, Ability>();
+    for (const ability of abilities) {
+      const index = ContainerComponent.index(ability);
+      const found = resultMap.get(index);
+      if (!found) {
+        resultMap.set(index, ability);
+      } else {
+        for (const intent of ability.intents) {
+          if (!found.intents.includes(intent)) {
+            found.intents.push(intent);
+          }
+        }
+        resultMap.set(index, found);
+      }
+    }
+    return Array.from(resultMap.values());
+  }
+
+  private static index(ability: Ability): string {
+    return `${ability.name}#${ability.host}:${ability.port}`;
+  }
+
   constructor(private service: AbilityService) {
   }
 
   ngOnInit(): void {
     this.service.getAbilities().subscribe(value => {
+      value.config = ContainerComponent.mergeByNameAndHostAndPort(value.config);
+      value.database = ContainerComponent.mergeByNameAndHostAndPort(value.database);
       this.abilities = value;
     }, error => {
       // console.log(error);
@@ -38,4 +66,6 @@ export class ContainerComponent implements OnInit {
   setToLink($event: Outlet): void {
     this.links[0] = {from: this.links[0].from, to: $event};
   }
+
+
 }
