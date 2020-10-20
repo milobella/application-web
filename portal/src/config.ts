4dd02d680c90978667d0034eb33040a289b56596
Celian Garcia <celian.garcia1@gmail.com>
2020-10-20 08:49:55 +0200

    fix: handle dev relocation on ewbpack configuration

--- portal/src/config.ts
+++ portal/src/config.ts
@@ -1,21 +1,7 @@
-export let MENU_APP_URL: string;
-export let TRY_IT_APP_URL: string;
-export let LOGIN_APP_URL: string;
-export let ABILITIES_APP_URL: string;
-export let NLG_APP_URL: string;
-
 const spaPathPrefix = '/spa';
 
-if (process.env.NODE_ENV === 'production') {
-  MENU_APP_URL = spaPathPrefix + '/menu';
-  TRY_IT_APP_URL = spaPathPrefix + '/try-it';
-  LOGIN_APP_URL = spaPathPrefix + '/login';
-  ABILITIES_APP_URL = spaPathPrefix + '/abilities';
-  NLG_APP_URL = spaPathPrefix + '/nlg';
-} else {
-  MENU_APP_URL = 'http://localhost:4201';
-  TRY_IT_APP_URL = 'http://localhost:4202';
-  LOGIN_APP_URL = 'http://localhost:4203';
-  ABILITIES_APP_URL = 'http://localhost:4204';
-  NLG_APP_URL = 'http://localhost:4205';
-}
+export const MENU_APP_URL = spaPathPrefix + '/menu';
+export const TRY_IT_APP_URL = spaPathPrefix + '/try-it';
+export const LOGIN_APP_URL = spaPathPrefix + '/login';
+export const ABILITIES_APP_URL = spaPathPrefix + '/abilities';
+export const NLG_APP_URL = spaPathPrefix + '/nlg';
