b677e5ce19c2a9a520ca88d45b1610d84ad517b2
Celian Garcia <celian.garcia1@gmail.com>
2020-11-08 09:49:45 +0100

    fix(#174) : Use single-spa-layout to ensure the position of applications

--- portal/src/main.ts
+++ portal/src/main.ts
@@ -1,16 +1,16 @@
 import { registerApplication, start } from 'single-spa';
 // import { constructApplications, constructLayoutEngine, constructRoutes } from 'single-spa-layout';
+const singleSpaLayout = require('single-spa-layout');
 
 import { loadAngularScript } from './loaders';
-import { MENU_APP_URL, TRY_IT_APP_URL, ABILITIES_APP_URL, NLG_APP_URL, LOGIN_APP_URL } from './config';
 
-const singleSpaLayout = require('single-spa-layout')
-const urls = {
-  "@milobella/menu": MENU_APP_URL,
-  "@milobella/try-it": TRY_IT_APP_URL,
-  "@milobella/abilities": ABILITIES_APP_URL,
-  "@milobella/nlg": NLG_APP_URL,
-  "@milobella/login": LOGIN_APP_URL,
+const spaPathPrefix = "/spa"
+const APPLICATIONS_URLS = {
+  "@milobella/menu": spaPathPrefix + '/menu',
+  "@milobella/try-it": spaPathPrefix + '/try-it',
+  "@milobella/nlg": spaPathPrefix + '/nlg',
+  "@milobella/abilities": spaPathPrefix + '/abilities',
+  "@milobella/login": spaPathPrefix + '/login',
 }
 
 const element = document.querySelector('#single-spa-layout')
@@ -20,55 +20,12 @@ if (element) {
     routes,
     loadApp: function (param : {name: string}) {
       const appName = param.name.replace("@milobella/", '')
-      return loadAngularScript(urls[param.name]).then(() => window[appName])
+      return loadAngularScript(APPLICATIONS_URLS[param.name]).then(() => window[appName])
     },
   });
   const layoutEngine = singleSpaLayout.constructLayoutEngine({ routes, applications });
   applications.forEach(registerApplication);
   start();
 }
-
-
-
-
-//
-//
-//
-//
-//
-//
-// registerApplication({
-//   name: 'spa-menu',
-//   app: () => loadAngularScript(MENU_APP_URL).then(() => window.menu),
-//   activeWhen: location => activeWhenAnyExcept(['/login'], location),
-// });
-//
-// registerApplication({
-//   name: 'spa-try-it',
-//   app: () => loadAngularScript(TRY_IT_APP_URL).then(() => window['try-it']),
-//   activeWhen: location => activeWhenAnyOf(['/', '/spa-try-it'], location),
-// });
-//
-// registerApplication({
-//   name: 'spa-abilities',
-//   app: () => loadAngularScript(ABILITIES_APP_URL).then(() => window.abilities),
-//   activeWhen: location => activeWhenAnyOf(['/spa-abilities'], location),
-// });
-//
-// registerApplication({
-//   name: 'spa-nlg',
-//   app: () => loadAngularScript(NLG_APP_URL).then(() => window.nlg),
-//   activeWhen: location => activeWhenAnyOf(['/spa-nlg'], location),
-// });
-//
-// registerApplication({
-//   name: 'spa-login',
-//   app: () => loadAngularScript(LOGIN_APP_URL).then(() => window.login),
-//   activeWhen: location => activeWhenPrefix(['/'], location),
-// });
-//
-// addErrorHandler(err => {
-//   console.log(err);
-// })
 
 
