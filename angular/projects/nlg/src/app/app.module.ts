9a44910888b3c22bf6bcd2b71cf6e47bbcf4d995
Celian Garcia <celian.garcia1@gmail.com>
2020-12-20 21:04:47 +0100

    fix: NLG sentences integration

--- angular/projects/nlg/src/app/app.module.ts
+++ angular/projects/nlg/src/app/app.module.ts
@@ -4,14 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SentencesListComponent } from 'projects/nlg/src/app/sentences-list/sentences-list.component';
+import { CommonModule } from '@angular/common';
+import { AssetUrlPipe } from 'projects/menu/src/app/pipes/asset-url.pipe';
+import { EmptyRouteComponent } from 'projects/menu/src/app/empty-route/empty-route.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SentencesListComponent
+    SentencesListComponent,
+    AssetUrlPipe,
+    EmptyRouteComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
